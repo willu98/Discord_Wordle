245c2800d569401b708c84fde9be72ec706f5f31
William <willu@my.yorku.ca>
2022-02-11 15:48:36 -0500

    scripts issue

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,11 +6,3 @@ sudo apt install nodejs
 node -v
 sudo apt install npm
 
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/wordle-bot"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
