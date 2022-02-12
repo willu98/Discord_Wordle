a54f8b25fcffe12e400ab1b00386b374ba88287e
William <willu@my.yorku.ca>
2022-02-12 03:43:39 -0500

    test

--- model/__tests__/game.test.js
+++ model/__tests__/game.test.js
@@ -1,7 +1,7 @@
 const game = require('../game');
 
 describe('my game', () => {
-    test('correct word map', () => {
+    test('correct word maps', () => {
         const testGame = game('TestID', 'exeem');
         expect(testGame.word).toBe('exeem');
     });
